6e1e006ddce60186bd7ee005a48288bb22d2df9d
Takuya Fukudome <wheelorder@gmail.com>
2019-05-01 01:47:10 +0900

    add sdl2

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,28 @@ use std::fs::File;
 use std::io::prelude::*;
 use byteorder::{ByteOrder, BigEndian};
 use resid::{ChipModel, Sid};
+use sdl2::audio::{AudioCallback, AudioSpecDesired};
+use std::time::Duration;
+
+struct SquareWave {
+    phase_inc: f32,
+    phase: f32,
+    volume: f32
+}
+
+impl AudioCallback for SquareWave {
+    type Channel = f32;
+
+    fn callback(&mut self, out: &mut [f32]) {
+        for x in out.iter_mut() {
+            *x = match self.phase {
+                v if v >= 0.0 && v < 0.5 => self.volume,
+                _ => - self.volume
+            };
+            self.phase = (self.phase + self.phase_inc) % 1.0;
+        }
+    }
+}
 
 #[derive(Debug)]
 struct Header {
@@ -98,5 +120,23 @@ fn main() -> std::io::Result<()> {
         }
         println!("{:?}", sid.read_state());
     }
+
+    // sdl2 audio
+    let sdl_context = sdl2::init().unwrap();
+    let audio_subsystem = sdl_context.audio().unwrap();
+    let desired_spec = AudioSpecDesired {
+        freq: Some(44100),
+        channels: Some(1),
+        samples: None
+    };
+    let device = audio_subsystem.open_playback(None, &desired_spec, |spec| {
+        SquareWave {
+            phase_inc: 440.0 / spec.freq as f32,
+            phase: 0.0,
+            volume: 0.25
+        }
+    }).unwrap();
+    device.resume();
+    std::thread::sleep(Duration::from_millis(2000));
     Ok(())
 }
